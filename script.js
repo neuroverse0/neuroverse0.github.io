f204711c28615d222a3d0ac70a9a032ac9cf31d8
danielkropf <danielkropf@uol.com.br>
2024-10-04 15:13:59 -0300

    Mais de 5 herois possiveis

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
+const heroesPerPage = 5;
+let currentPage = 1;
+let totalPages = 0;
+const groups = [];
+
 fetch('heroes.json')
   .then(response => response.json())
   .then(data => {
+
+    for (let i = 0; i < data.length; i += heroesPerPage) {
+      groups.push(data.slice(i, i + heroesPerPage));
+    }
+    totalPages = groups.length;
+
     const buttons = document.querySelectorAll('.hero_btn');
 
-    buttons.forEach((button, index) => {
-      button.textContent = data[index].nome;
+    updateButtons(currentPage);
+
+    document.getElementById('btnPrev').addEventListener('click', () => {
+      if (currentPage > 1) {
+        updateButtons(currentPage - 1);
+      }
+    });
+    
+    document.getElementById('btnNext').addEventListener('click', () => {
+      if (currentPage < totalPages) {
+        updateButtons(currentPage + 1);
+      }
+    });
+  });
+
+function updateButtons(page) {
+  currentPage = page;
+  const startIndex = (page - 1) * heroesPerPage;
+  const endIndex = startIndex + heroesPerPage;
+  const buttons = document.querySelectorAll('.hero_btn');
+
+  buttons.forEach((button, index) => {
+    const heroes = groups[page - 1];
+    if(index < heroes.length) {
+      button.textContent = heroes[index].nome;
 
       button.addEventListener('click', () => {
-        const heroData = data[index];
+        const heroData = heroes[index];
         document.getElementById("id").textContent = heroData.id
         document.getElementById("origem").textContent = heroData.origem
         document.getElementById("poder").textContent = heroData.poder
@@ -21,5 +55,13 @@ fetch('heroes.json')
         // Adiciona a classe 'active' ao botão clicado
         button.classList.add('active');
       })
-    });
+      button.disabled = false;
+
+    } else {
+      button.textContent = '---';
+      button.disabled = true;
+    }
   });
+
+  document.getElementById('page').textContent = currentPage;
+}
