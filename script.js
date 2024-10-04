fe7e0622c7285a0e8302942619e7ae2374fad038
danielkropf <danielkropf@uol.com.br>
2024-10-04 12:22:06 -0300

    Termino do minimo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch('heroes.json')
     const buttons = document.querySelectorAll('.hero_btn');
 
     buttons.forEach((button, index) => {
-      button.textContent = data[index].name;
+      button.textContent = data[index].nome;
 
       button.addEventListener('click', () => {
         const heroData = data[index];
@@ -12,8 +12,14 @@ fetch('heroes.json')
         document.getElementById("origem").textContent = heroData.origem
         document.getElementById("poder").textContent = heroData.poder
         document.getElementById("inimigo").textContent = heroData.inimigo
-        document.getElementById("image").textContent = heroData.imagem
+        document.getElementById("image").src = heroData.imagem
         document.getElementById("hero_name").textContent = heroData.nome
+
+        // Remove a classe 'active' de todos os botões
+        buttons.forEach(btn => btn.classList.remove('active'));
+
+        // Adiciona a classe 'active' ao botão clicado
+        button.classList.add('active');
       })
     });
   });
