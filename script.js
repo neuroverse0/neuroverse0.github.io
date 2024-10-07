627f294b93457a1ab3406438e66b85fab9742aa9
danielkropf <danielkropf@uol.com.br>
2024-10-07 14:46:28 -0300

    Carrossel das imagens e correções no dimensionamento

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const heroesPerPage = 5;
 let currentPage = 1;
 let totalPages = 0;
+let selectedHeroID = null;
 const groups = [];
 
 fetch('heroes.json')
@@ -35,19 +36,27 @@ function updateButtons(page) {
   const endIndex = startIndex + heroesPerPage;
   const buttons = document.querySelectorAll('.hero_btn');
 
+  buttons.forEach(btn => btn.classList.remove('active'));
+
   buttons.forEach((button, index) => {
     const heroes = groups[page - 1];
     if(index < heroes.length) {
       button.textContent = heroes[index].nome;
+      button.dataset.heroId = heroes[index].id;
+      button.dataset.images = JSON.stringify(heroes[index].imagem);
 
       button.addEventListener('click', () => {
         const heroData = heroes[index];
-        document.getElementById("id").textContent = heroData.id
-        document.getElementById("origem").textContent = heroData.origem
-        document.getElementById("poder").textContent = heroData.poder
-        document.getElementById("inimigo").textContent = heroData.inimigo
-        document.getElementById("image").src = heroData.imagem
-        document.getElementById("hero_name").textContent = heroData.nome
+        selectedHeroID = heroData.id;
+
+        document.getElementById("id").textContent = heroData.id;
+        document.getElementById("origem").textContent = heroData.origem;
+        document.getElementById("poder").textContent = heroData.poder;
+        document.getElementById("inimigo").textContent = heroData.inimigo;
+        document.getElementById("image").src = heroData.imagem[0];
+        document.getElementById("image").dataset.imageActive = 0;
+        document.getElementById("image").dataset.images = JSON.stringify(heroData.imagem);
+        document.getElementById("hero_name").textContent = heroData.nome;
 
         // Remove a classe 'active' de todos os botões
         buttons.forEach(btn => btn.classList.remove('active'));
@@ -63,5 +72,37 @@ function updateButtons(page) {
     }
   });
 
+  if(selectedHeroID) {
+    buttons.forEach(button => {
+      const heroData = button.dataset.heroId;
+      if(heroData === selectedHeroID) {
+        button.classList.add('active');
+      }
+    })
+  }
+
   document.getElementById('page').textContent = currentPage;
+}
+
+function nextImage() {
+  const image = document.getElementById("image");
+  const images = JSON.parse(image.dataset.images);
+
+  if(parseInt(image.dataset.imageActive) + 1 < images.length) {
+    image.dataset.imageActive = parseInt(image.dataset.imageActive) + 1;
+    image.src = images[image.dataset.imageActive];
+  }
+
+  console.log(images);
+  console.log(images.dataset.imageActive);
+}
+
+function prevImage() {
+  const image = document.getElementById("image");
+  const images = JSON.parse(image.dataset.images);
+
+  if(parseInt(image.dataset.imageActive) > 0) {
+    image.dataset.imageActive = parseInt(image.dataset.imageActive) - 1;
+    image.src = images[image.dataset.imageActive];
+  }
 }
